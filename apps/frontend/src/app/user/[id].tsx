9ea85c75f87a899de0f15da99f731bcf4657fcf6
Kartik Dixit <Kartikdixit2468@gmail.com>
2026-09-02 17:15:55 +0530

    Bug Fix: Fixed incorrect redirection point

--- apps/frontend/src/app/user/[id].tsx
+++ apps/frontend/src/app/user/[id].tsx
@@ -77,7 +77,7 @@ export default function UserProfileScreen() {
     if (router.canGoBack()) {
       router.back();
     } else {
-      router.replace("/(tabs)/activity");
+      router.replace("/(tabs)/activ");
     }
   };
 
